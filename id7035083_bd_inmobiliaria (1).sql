045825ed7bd77398c2d8c7cfc943a6cb64f55f56
JanetzinCastro <zalmajanetzincastro@outlook.com>
2018-12-09 21:26:35 -0600

    Update id7035083_bd_inmobiliaria (1).sql
    Se agrego una tabla para el login de facebook

--- id7035083_bd_inmobiliaria (1).sql
+++ id7035083_bd_inmobiliaria (1).sql
@@ -1,9 +1,9 @@
-﻿-- phpMyAdmin SQL Dump
+-- phpMyAdmin SQL Dump
 -- version 4.7.7
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: localhost:3306
--- Tiempo de generación: 02-12-2018 a las 18:40:44
+-- Tiempo de generación: 10-12-2018 a las 03:23:29
 -- Versión del servidor: 10.2.12-MariaDB
 -- Versión de PHP: 7.0.26
 
@@ -19,14 +19,14 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Base de datos: CREATE DATABASE `id7035083_bd_inmobiliaria`;USE `id7035083_bd_inmobiliaria`;
+-- Base de datos: `id7035083_bd_inmobiliaria`
 --
 
 DELIMITER $$
 --
 -- Procedimientos
 --
-CREATE PROCEDURE `spBajaTerreno` (IN `id` INT)  BEGIN
+CREATE DEFINER=`id7035083_pepe`@`%` PROCEDURE `spBajaTerreno` (IN `id` INT)  BEGIN
 	IF EXISTS (SELECT * FROM INM_TERRENO WHERE TER_CVE = id) THEN
     BEGIN
 		UPDATE INM_TERRENO SET TER_ESTATUS = 0
@@ -41,7 +41,7 @@ CREATE PROCEDURE `spBajaTerreno` (IN `id` INT)  BEGIN
 
 END$$
 
-CREATE PROCEDURE `spBuscarTerrenos` (IN `ESTADO` VARCHAR(50), IN `TIPO` VARCHAR(50))  BEGIN
+CREATE DEFINER=`id7035083_pepe`@`%` PROCEDURE `spBuscarTerrenos` (IN `ESTADO` VARCHAR(50), IN `TIPO` VARCHAR(50))  BEGIN
 IF (ESTADO!='' OR TIPO!='')THEN
   BEGIN
 	SELECT A.TER_CVE ID, CONCAT(A.TER_CALLE,' ', A.TERR_COLONIA,' ', A.TER_MUNICIPIO,' ', A.TER_ESTADO)
@@ -75,7 +75,7 @@ WHERE TER_ESTATUS = 1  AND TER_TIPO_CVE = TIPO_CVE  ORDER BY TER_FECHA_REG;
     END IF;
 END$$
 
-CREATE PROCEDURE `spEditarPerfil` (IN `id` INT, IN `NOMUSU` VARCHAR(50), IN `CON` VARCHAR(50), IN `TELEFONO` VARCHAR(50), IN `EMAIL` VARCHAR(80), IN `CALLE` VARCHAR(50), IN `COLONIA` VARCHAR(80), IN `MUNICIPIO` VARCHAR(50), IN `ESTADO` VARCHAR(50), IN `PAGOCVE` VARCHAR(50))  BEGIN
+CREATE DEFINER=`id7035083_pepe`@`%` PROCEDURE `spEditarPerfil` (IN `id` INT, IN `NOMUSU` VARCHAR(50), IN `CON` VARCHAR(50), IN `TELEFONO` VARCHAR(50), IN `EMAIL` VARCHAR(80), IN `CALLE` VARCHAR(50), IN `COLONIA` VARCHAR(80), IN `MUNICIPIO` VARCHAR(50), IN `ESTADO` VARCHAR(50), IN `PAGOCVE` VARCHAR(50))  BEGIN
 IF EXISTS(SELECT USU_CVE_ID FROM INM_USUARIO  WHERE USU_CVE_ID = id) THEN
 BEGIN
 UPDATE INM_PERFIL, INM_USUARIO SET PER_TELEFONO = TELEFONO, 
@@ -91,7 +91,7 @@ SELECT "0" CLAVE;
 END IF;
 END$$
 
-CREATE PROCEDURE `spEditarTerreno` (IN `id` INT, IN `CALLE` VARCHAR(50), IN `COLONIA` VARCHAR(50), IN `MUNICIPIO` VARCHAR(50), IN `ESTADO` VARCHAR(50), IN `LARGO` VARCHAR(50), IN `ANCHO` VARCHAR(50), IN `PRECIO` VARCHAR(50), IN `DESCRIPCION` VARCHAR(500), IN `TIPOCVE` VARCHAR(50))  BEGIN
+CREATE DEFINER=`id7035083_pepe`@`%` PROCEDURE `spEditarTerreno` (IN `id` INT, IN `CALLE` VARCHAR(50), IN `COLONIA` VARCHAR(50), IN `MUNICIPIO` VARCHAR(50), IN `ESTADO` VARCHAR(50), IN `LARGO` VARCHAR(50), IN `ANCHO` VARCHAR(50), IN `PRECIO` VARCHAR(50), IN `DESCRIPCION` VARCHAR(500), IN `TIPOCVE` VARCHAR(50))  BEGIN
 IF EXISTS(SELECT TER_CVE FROM INM_TERRENO  WHERE TER_CVE = id) THEN
 BEGIN
 UPDATE INM_TERRENO SET TER_CALLE = CALLE,TERR_COLONIA = COLONIA,
@@ -108,7 +108,7 @@ SELECT "0" CLAVE;
 END IF;
 END$$
 
-CREATE PROCEDURE `spEliminarTerreno` (IN `id` INT)  BEGIN
+CREATE DEFINER=`id7035083_pepe`@`%` PROCEDURE `spEliminarTerreno` (IN `id` INT)  BEGIN
 	IF EXISTS (SELECT * FROM INM_TERRENO  WHERE TER_CVE = id) THEN
     BEGIN
 		DELETE FROM INM_TERRENO 
@@ -122,7 +122,7 @@ CREATE PROCEDURE `spEliminarTerreno` (IN `id` INT)  BEGIN
 
 END$$
 
-CREATE PROCEDURE `spEliminarUsuario` (IN `id` INT)  BEGIN
+CREATE DEFINER=`id7035083_pepe`@`%` PROCEDURE `spEliminarUsuario` (IN `id` INT)  BEGIN
 	IF EXISTS (SELECT * FROM INM_USUARIO WHERE USU_CVE_ID = id) THEN
     BEGIN
 		DELETE FROM INM_USUARIO 
@@ -136,7 +136,7 @@ CREATE PROCEDURE `spEliminarUsuario` (IN `id` INT)  BEGIN
 
 END$$
 
-CREATE PROCEDURE `spMostrarDatos` (IN `id` INT)  BEGIN
+CREATE DEFINER=`id7035083_pepe`@`%` PROCEDURE `spMostrarDatos` (IN `id` INT)  BEGIN
 
 IF EXISTS(SELECT PER_CVE FROM INM_PERFIL  WHERE PER_CVE = id) THEN
 
@@ -150,7 +150,7 @@ SELECT "0" CV;
 END IF;
 END$$
 
-CREATE PROCEDURE `spMostrarPerfil` (IN `id` INT)  BEGIN
+CREATE DEFINER=`id7035083_pepe`@`%` PROCEDURE `spMostrarPerfil` (IN `id` INT)  BEGIN
 
 IF EXISTS(SELECT USU_CVE_ID FROM INM_USUARIO  WHERE USU_CVE_ID = id) THEN
 
@@ -171,7 +171,7 @@ SELECT "0" CV;
 END IF;
 END$$
 
-CREATE PROCEDURE `spMostrarTerreno` (IN `id` INT)  BEGIN
+CREATE DEFINER=`id7035083_pepe`@`%` PROCEDURE `spMostrarTerreno` (IN `id` INT)  BEGIN
 
 IF EXISTS(SELECT TER_USU_CVE FROM INM_TERRENO WHERE TER_USU_CVE = id) 
 THEN
@@ -189,7 +189,7 @@ SELECT "0" CLAVE;
 END IF;
 END$$
 
-CREATE PROCEDURE `spMostrarTerrenosReg` (IN `id` INT)  BEGIN
+CREATE DEFINER=`id7035083_pepe`@`%` PROCEDURE `spMostrarTerrenosReg` (IN `id` INT)  BEGIN
 
 IF EXISTS(SELECT USU_CVE_ROL FROM INM_USUARIO WHERE USU_CVE_ID = id AND USU_CVE_ROL = 1 ) 
 THEN
@@ -215,7 +215,7 @@ SELECT "0" CLAVE;
 END IF;
 END$$
 
-CREATE PROCEDURE `spMostrarUsuario` (IN `id` INT)  BEGIN
+CREATE DEFINER=`id7035083_pepe`@`%` PROCEDURE `spMostrarUsuario` (IN `id` INT)  BEGIN
 
 IF EXISTS(SELECT USU_CVE_ID FROM INM_USUARIO  WHERE USU_CVE_ID = id) THEN
 
@@ -229,7 +229,7 @@ SELECT "0" CV;
 END IF;
 END$$
 
-CREATE PROCEDURE `spMostrarUsuariosReg` (IN `id` INT)  BEGIN
+CREATE DEFINER=`id7035083_pepe`@`%` PROCEDURE `spMostrarUsuariosReg` (IN `id` INT)  BEGIN
 
 IF EXISTS(SELECT USU_CVE_ROL FROM INM_USUARIO WHERE USU_CVE_ID = id AND USU_CVE_ROL = 1) 
 THEN
@@ -248,7 +248,7 @@ SELECT "0" CLAVE;
 END IF;
 END$$
 
-CREATE PROCEDURE `spRegistrarAdmin` (IN `NOMBRE` VARCHAR(50), IN `APELLIDOPAT` VARCHAR(50), IN `APELLIDOMAT` VARCHAR(50), IN `NOMUSU` VARCHAR(50), IN `PASS` VARCHAR(50))  BEGIN
+CREATE DEFINER=`id7035083_pepe`@`%` PROCEDURE `spRegistrarAdmin` (IN `NOMBRE` VARCHAR(50), IN `APELLIDOPAT` VARCHAR(50), IN `APELLIDOMAT` VARCHAR(50), IN `NOMUSU` VARCHAR(50), IN `PASS` VARCHAR(50))  BEGIN
 	IF NOT EXISTS (SELECT * FROM INM_USUARIO
 		WHERE USU_USUARIO = NOMUSU) THEN
 	BEGIN
@@ -260,7 +260,7 @@ CREATE PROCEDURE `spRegistrarAdmin` (IN `NOMBRE` VARCHAR(50), IN `APELLIDOPAT` V
 	END IF;
 END$$
 
-CREATE PROCEDURE `spRegistrarPerfil` (IN `TELEFONO` VARCHAR(50), IN `EMAIL` VARCHAR(80), IN `CALLE` VARCHAR(50), IN `COLONIA` VARCHAR(80), IN `MUNICIPIO` VARCHAR(50), IN `ESTADO` VARCHAR(50), IN `USUCVE` VARCHAR(50), IN `PAGOCVE` VARCHAR(50))  MODIFIES SQL DATA
+CREATE DEFINER=`id7035083_pepe`@`%` PROCEDURE `spRegistrarPerfil` (IN `TELEFONO` VARCHAR(50), IN `EMAIL` VARCHAR(80), IN `CALLE` VARCHAR(50), IN `COLONIA` VARCHAR(80), IN `MUNICIPIO` VARCHAR(50), IN `ESTADO` VARCHAR(50), IN `USUCVE` VARCHAR(50), IN `PAGOCVE` VARCHAR(50))  MODIFIES SQL DATA
 BEGIN
 	IF NOT EXISTS (SELECT * FROM INM_PERFIL
 		WHERE PER_EMAIL = EMAIL) THEN
@@ -273,7 +273,7 @@ BEGIN
 	END IF;
 END$$
 
-CREATE PROCEDURE `spRegistrarTerreno` (IN `CALLE` VARCHAR(50), IN `COLONIA` VARCHAR(50), IN `MUNICIPIO` VARCHAR(50), IN `ESTADO` VARCHAR(50), IN `LARGO` VARCHAR(50), IN `ANCHO` VARCHAR(50), IN `PRECIO` VARCHAR(50), IN `DESCRIPCION` VARCHAR(500), IN `USUCVE` VARCHAR(50), IN `TIPOCVE` VARCHAR(50))  BEGIN
+CREATE DEFINER=`id7035083_pepe`@`%` PROCEDURE `spRegistrarTerreno` (IN `CALLE` VARCHAR(50), IN `COLONIA` VARCHAR(50), IN `MUNICIPIO` VARCHAR(50), IN `ESTADO` VARCHAR(50), IN `LARGO` VARCHAR(50), IN `ANCHO` VARCHAR(50), IN `PRECIO` VARCHAR(50), IN `DESCRIPCION` VARCHAR(500), IN `USUCVE` VARCHAR(50), IN `TIPOCVE` VARCHAR(50))  BEGIN
 	IF NOT EXISTS (SELECT * FROM INM_TERRENO
 		WHERE TER_CALLE = CALLE 
         AND TERR_COLONIA = COLONIA 
@@ -289,7 +289,7 @@ CREATE PROCEDURE `spRegistrarTerreno` (IN `CALLE` VARCHAR(50), IN `COLONIA` VARC
 	END IF;
 END$$
 
-CREATE PROCEDURE `spRegistrarUsuarios` (IN `NOMBRE` VARCHAR(50), IN `APELLIDOPAT` VARCHAR(50), IN `APELLIDOMAT` VARCHAR(50), IN `NOMUSU` VARCHAR(50), IN `PASS` VARCHAR(50))  MODIFIES SQL DATA
+CREATE DEFINER=`id7035083_pepe`@`%` PROCEDURE `spRegistrarUsuarios` (IN `NOMBRE` VARCHAR(50), IN `APELLIDOPAT` VARCHAR(50), IN `APELLIDOMAT` VARCHAR(50), IN `NOMUSU` VARCHAR(50), IN `PASS` VARCHAR(50))  MODIFIES SQL DATA
 BEGIN
 	IF NOT EXISTS (SELECT * FROM INM_USUARIO
 		WHERE USU_USUARIO = NOMUSU) THEN
@@ -302,7 +302,7 @@ BEGIN
 	END IF;
 END$$
 
-CREATE PROCEDURE `spValidarAccesoAdm` (IN `usuario` VARCHAR(50), IN `contra` VARCHAR(50))  BEGIN
+CREATE DEFINER=`id7035083_pepe`@`%` PROCEDURE `spValidarAccesoAdm` (IN `usuario` VARCHAR(50), IN `contra` VARCHAR(50))  BEGIN
 IF EXISTS(SELECT * FROM INM_USUARIO WHERE USU_USUARIO=usuario AND USU_CONTRASENA = contra) THEN 
 SELECT A.USU_CVE_ID CLAVE, concat(A.USU_NOMBRE," ",A.USU_APELLIDO_PATERNO," ",A.USU_APELLIDO_MATERNO) NOMBRE,
 B.ROL_NOMBRE ROL
@@ -349,7 +349,7 @@ INSERT INTO `INM_ESTADO` (`ESTADO_CVE`, `ESTADO_NOMBRE`) VALUES
 (13, 'Hidalgo'),
 (14, 'Jalisco'),
 (15, 'Mexico'),
-(16, 'Michoacán'),
+(16, 'Michoacan'),
 (17, 'Morelos'),
 (18, 'Nayarit'),
 (19, 'Nuevo Leon'),
@@ -364,7 +364,7 @@ INSERT INTO `INM_ESTADO` (`ESTADO_CVE`, `ESTADO_NOMBRE`) VALUES
 (28, 'Tamaulipas'),
 (29, 'Tlaxcala'),
 (30, 'Veracruz'),
-(31, 'Yucatan'),
+(31, 'Yucatán'),
 (32, 'Zacatecas');
 
 -- --------------------------------------------------------
@@ -397,6 +397,15 @@ CREATE TABLE `INM_PERFIL` (
   `PER_USU_CVE_ID` int(11) DEFAULT NULL,
   `PER_PAGO_CVE` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Volcado de datos para la tabla `INM_PERFIL`
+--
+
+INSERT INTO `INM_PERFIL` (`PER_CVE`, `PER_TELEFONO`, `PER_EMAIL`, `PER_CALLE`, `PER_COLONIA`, `PER_MUNICIPIO`, `PER_ESTADO`, `PER_USU_CVE_ID`, `PER_PAGO_CVE`) VALUES
+(1, '5512345678', 'jessica@mail.com', 'Fernando Montes de Oca', 'Sto. Tomas', 'Tizayuca', 'Hidalgo', 8, 5),
+(2, '7711234567', 'maria@gmail.com', 'Sur #1', 'Hidalgo', 'Gomez Farias', 'Coahuila', 5, 4),
+(3, '552468010', 'alberto@gmail.com', 'Sto Domingo', 'Barrio Alto', 'Zacatecas', 'Zacatecas', 6, 3);
 
 -- --------------------------------------------------------
 
@@ -443,6 +452,20 @@ CREATE TABLE `INM_TERRENO` (
   `TER_USU_CVE` int(11) DEFAULT NULL,
   `TER_TIPO_CVE` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Volcado de datos para la tabla `INM_TERRENO`
+--
+
+INSERT INTO `INM_TERRENO` (`TER_CVE`, `TER_CALLE`, `TERR_COLONIA`, `TER_MUNICIPIO`, `TER_ESTADO`, `TER_LARGO`, `TER_ANCHO`, `TER_TOTAL`, `TER_PRECIO`, `TER_DESC`, `TER_ESTATUS`, `TER_FECHA_REG`, `TER_USU_CVE`, `TER_TIPO_CVE`) VALUES
+(1, 'Norte #3', 'Lomas Altas', 'Ecatepec', 'Mexico', '120', '40', '4800', '50000', 'Terreno amplio apto para bodega', 1, '2018-12-03 08:44:06', 8, 6),
+(2, 'Zurich', 'Mazarik', 'Benito Juarez', 'CDMX', '20', '40', '800', '120000', 'Excelente Ubicacion', 1, '2018-12-03 08:45:33', 8, 2),
+(3, 'Juarez #34', 'Periodistas', 'Cancun', 'Quintana Roo', '120', '132', '15840', '120000', 'Terreno a la orilla de la playa', 1, '2018-12-03 15:34:21', 5, 3),
+(4, 'Fernando Montes de Oca #4', 'Centro', 'Juarez', 'Tamaulipas', '132', '15', '1980', '530000', 'Departamento amplio, permite mascotas', 1, '2018-12-03 15:36:03', 5, 2),
+(5, 'Benito Juarez #7', 'Norte 2', 'Cuautla', 'Morelos', '50', '50', '2500', '36000', 'Excelente lugar para almacenar productos agricolas', 1, '2018-12-03 15:38:13', 5, 6),
+(6, 'Niños Heroes', 'Barrio San Martin', 'Colima', 'Colima', '30', '20', '600', '156000', 'Rento local comercial', 0, '2018-12-03 15:45:58', 6, 5),
+(7, 'San Antonio #5', 'Villas de Hermosillo', 'Hermosillo', 'Sonora', '100', '200', '20000', '200000', 'Rento oficinas para diferentes despachos', 1, '2018-12-03 15:45:33', 6, 4),
+(8, 'Sto Domingo', 'Norte 2', 'San Ignacio', 'Oaxaca', '560', '450', '252000', '120000', 'Vendo oficina para consultorio o despacho medico', 1, '2018-12-03 16:17:41', 6, 4);
 
 -- --------------------------------------------------------
 
@@ -510,6 +533,44 @@ CREATE TABLE `INM_USUARIO` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
+-- Volcado de datos para la tabla `INM_USUARIO`
+--
+
+INSERT INTO `INM_USUARIO` (`USU_CVE_ID`, `USU_CVE_ROL`, `USU_NOMBRE`, `USU_APELLIDO_PATERNO`, `USU_APELLIDO_MATERNO`, `USU_USUARIO`, `USU_CONTRASENA`, `USU_ESTATUS`, `USU_FECHA_REGISTRO`) VALUES
+(1, 1, 'Zalma', 'Castro', 'Rosas', 'Zalma', 'zalma123', 1, '2018-12-03 08:28:43'),
+(2, 1, 'Alicia', 'Martinez', 'Martinez', 'Alicia', 'alicia123', 1, '2018-12-03 08:28:43'),
+(3, 1, 'Angel', 'Ambriz', 'Hernandez', 'Angel', 'angel123', 1, '2018-12-03 08:28:43'),
+(4, 1, 'Fredy', 'Solis', 'Portilla', 'Fredy', 'fredy123', 1, '2018-12-03 08:28:43'),
+(5, 2, 'Maria', 'Lopez', 'Garcia', 'Maria', 'maria123', 1, '2018-12-03 08:36:16'),
+(6, 2, 'Alberto', 'Cruz', 'Teofilo', 'Alberto', 'alberto123', 1, '2018-12-03 08:36:16'),
+(7, 2, 'Luis', 'Gonzalez', 'Lechuga', 'Luis', 'luis123', 1, '2018-12-03 08:36:16'),
+(8, 2, 'Jessica', 'Elizalde', 'Ortega', 'Jessica', 'jessica123', 1, '2018-12-03 08:36:16'),
+(9, 2, 'Eduardo', 'Martinez', 'Perez', 'Eduardo', 'eduardo123', 1, '2018-12-03 08:36:16'),
+(10, 2, 'Mario', 'Moreno', 'Alarcon', 'Mario', 'mario123', 1, '2018-12-03 15:24:00'),
+(11, 2, 'Marco', 'Acosta', 'Lopez', 'Marco', '12345', 1, '2018-12-08 07:22:06');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `oauth_provider` enum('','facebook','google','twitter') COLLATE utf8_unicode_ci NOT NULL,
+  `oauth_uid` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `first_name` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  `last_name` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  `email` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `gender` varchar(10) COLLATE utf8_unicode_ci NOT NULL,
+  `locale` varchar(10) COLLATE utf8_unicode_ci NOT NULL,
+  `picture` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `link` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `created` datetime NOT NULL,
+  `modified` datetime NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
 -- Índices para tablas volcadas
 --
 
@@ -562,6 +623,12 @@ ALTER TABLE `INM_USUARIO`
   ADD PRIMARY KEY (`USU_CVE_ID`);
 
 --
+-- Indices de la tabla `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- AUTO_INCREMENT de las tablas volcadas
 --
 
@@ -581,7 +648,7 @@ ALTER TABLE `INM_FOTO`
 -- AUTO_INCREMENT de la tabla `INM_PERFIL`
 --
 ALTER TABLE `INM_PERFIL`
-  MODIFY `PER_CVE` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+  MODIFY `PER_CVE` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT de la tabla `INM_ROL`
@@ -611,9 +678,16 @@ ALTER TABLE `INM_TIPO_TERRENO`
 -- AUTO_INCREMENT de la tabla `INM_USUARIO`
 --
 ALTER TABLE `INM_USUARIO`
-  MODIFY `USU_CVE_ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+  MODIFY `USU_CVE_ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+
+--
+-- AUTO_INCREMENT de la tabla `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+
